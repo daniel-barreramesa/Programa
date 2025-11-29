2e1d0ef14e93ce223e85408b5285a45ba0d73fff
daniel-barreramesa <alu0101742830@ull.edu.es>
2025-11-29 10:11:47 +0000

    v0.1.1

---
+++ src/main.cc
@@ -0,0 +1,23 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica 2025-2026
+ *
+ * @file escribir.cc
+ * @author Daniel Barrera Mesa alu0101742830@ull.edu.es
+ * @date Nov 26 2025
+ * @brief Escribe un archivo de texto según la entrada del usuario
+ * @bug There are no known bugs
+ * @see https://jutge.org/problems/P48107
+ */
+
+ #include<iostream>
+ #include"producto.cc"
+
+ int main(){
+  Producto p1 = Producto("Producto", 10, )
+
+
+  return 0;
+ }
---
+++ src/producto.cc
@@ -0,0 +1,39 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica 2025-2026
+ *
+ * @file escribir.cc
+ * @author Daniel Barrera Mesa alu0101742830@ull.edu.es
+ * @date Nov 28 2025
+ * @brief Escribe un archivo de texto según la entrada del usuario
+ * @bug There are no known bugs
+ * @see https://jutge.org/problems/P48107
+ */
+#include<iostream>
+#include<string>
+#include<vector>
+
+class Producto{
+  private: //atributos
+    std::string nombre_producto;
+    int precio;
+    int referencia;
+    int stock;
+    std::string descripción_producto;
+    std::vector<int> fecha_creación(3);
+
+  public: //métodos
+    Producto(std::string, int, int, int, std::string, std::vector<int> );// constructor
+  }
+
+//Constructor
+Producto::Producto(std::string nombre_producto,int precio,int referencia,int stock,std::string descripción_producto,std::vector<int> fecha_creación(3)){
+  this.nombre_producto = nombre_producto;
+  this.precio = precio;
+  this.referencia = referencia;
+  this.stock = stock;
+  this.descripción_producto = descripción_producto;
+  this.fecha_creación = fecha_creación;  
+}
